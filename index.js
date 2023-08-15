4df58b1bff2934a054dd8e57c78bc8c50a978383
Replit user <>
2023-08-15 21:47:27 +0000

    added pages

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
-var http = require("http");
-http.createServer(function(req,res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Aloha world');
-}).listen(process.env.PORT || 3000);
+const express = require('express')
+const app = express();
+const port = 3000;
+
+//EJS is our view engine
+app.set("view engine", "ejs");
+
+//this will allow us to serve up static files, CSS, images & JS
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+  let title = "Home";
+  res.render("pages/index", { "title": title });
+});
+
+app.get('/about', (req, res) => {
+  let title = "About Page";
+  res.render("pages/about", { "title": title });
+});
+
+app.get('/kdrama', (req, res) => {
+  let title = "Kdrama Recommendations";
+  res.render("pages/kdrama", { "title": title });
+});
+
+app.get('/crochet', (req, res) => {
+  let title = "Crochet";
+  res.render("pages/crochet", { "title": title });
+});
+
+app.get('/games', (req, res) => {
+  let title = "Games";
+  res.render("pages/games", { "title": title });
+});
+
+app.get('/goals', (req, res) => {
+  let title = "Future Goals";
+  res.render("pages/goals", { "title": title });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
