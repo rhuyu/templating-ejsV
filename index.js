280483a8aba54524b5d4e404908e0fbe97d94151
Replit user <>
2023-08-18 07:24:25 +0000

    list view

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express();
 const port = 3000;
+var data = require('./data/test.json');
+
 
 //EJS is our view engine
 app.set("view engine", "ejs");
@@ -38,6 +40,29 @@ app.get('/goals', (req, res) => {
   res.render("pages/goals", { "title": title });
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+//users index is our list page
+app.get('/users', function(req, res) {
+	var title = 'Users Page';
+	res.render('users/index', {
+    	title: title,
+    	users: data
+	});
 });
+
+//add user/view route - we are cheating by using the array index - 1
+app.get('/users/view/:id', function(req, res) {
+ var title = 'User Page';
+ var id = req.params.id;
+ res.render('users/view', {
+     title: title,
+     user: data[--id]
+ });
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+  console.log(data);
+});
+
